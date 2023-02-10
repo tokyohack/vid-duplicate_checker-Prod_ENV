85fd99636e4c876fedff5ccaa1bea81df70df003
tokyohack <126138e2636n@gmail.com>
2023-02-10 16:47:26 +0900

    コード抜け追加 引数が2個以上の場合終了する

--- src/main/java/video/checkArgs.java
+++ src/main/java/video/checkArgs.java
@@ -1,25 +1,32 @@
 // 引数ファイルパスの確認
 package video;
+
 import java.io.File;
+
 public class checkArgs {
-    private  String nameFilePath;
+    private String nameFilePath;
+
     public checkArgs(String[] args) {
         if (isArgsNull(args)) {
             var filePath = this.checkExistsFilePath(args[0]);
             if (filePath != null) {
-                this.nameFilePath =filePath;
+                this.nameFilePath = filePath;
             }
         } else {
             System.out.println("引数が無いか2個以上入っています");
+            System.exit(0);
         }
     }
+
     public String getterFilePath() {
         return this.nameFilePath;
     }
-//     引数が無い場合か引数が1以外の時Falseを返す
+
+    //     引数が無い場合か引数が1以外の時Falseを返す
     public boolean isArgsNull(String[] args) {
         return !(args == null || args.length != 1);
     }
+
     public String checkExistsFilePath(String vidPath) {
         File fPath = new File(vidPath);
         if (fPath.exists() && !fPath.isDirectory()) {
--- src/main/java/video/isUnique.java
+++ src/main/java/video/isUnique.java
@@ -1,14 +1,19 @@
 // DBに重複があるかbool返す
 package video;
+
 import java.util.Objects;
+
 public class isUnique {
     private boolean bRet;
+
     public isUnique(Object nameForsql, String nameForArg, Object sizeForsql, long sizeForArg, Object dateForSql, long dateForArg) {
         this.setterIsUnique(Objects.equals((String) nameForsql, nameForArg) && (long) sizeForsql == sizeForArg && (long) dateForSql == dateForArg);
     }
+
     private void setterIsUnique(boolean bRet) {
         this.bRet = bRet;
     }
+
     public boolean getterIsUnique() {
         return this.bRet;
     }
